f3efc6267eeab226ebceb6be44d22bcaf6242126
Abdullah06444 <abdullah44gur@gmail.com>
2020-08-23 02:44:35 +0300

    Added CSharp Code

---
+++ CSharp0001_HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharp0001_HelloWorld
+{
+    class CSharp0001_HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I started to learn CSharp programming language. :D");
+        }
+    }
+}
